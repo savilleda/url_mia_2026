84e558fa2b01cfc981b50f04f51827b9a99f6f10
Sebastian Villeda <savilleda@correo.url.edu.gt>
2026-08-26 07:59:59 -0600

    feat: agregando archivos que no había subido

--- Lab5/obj/Debug/net10.0/Lab5.AssemblyInfo.cs
+++ Lab5/obj/Debug/net10.0/Lab5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a986fcb4b79b620979e26db338387afe4295737")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03aa96e0076792dc1fb170f24232f60cd244bfc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
